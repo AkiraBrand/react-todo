d90213431133098b5e1836098d4af81802147483
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-08-31 18:30:12 -0700

    adds ability to make new todo

--- src/App.js
+++ src/App.js
@@ -18,6 +18,24 @@ function App() {
     },
   ]);
 
+  function handleKeyDown(e, i) {
+    if (e.key === "Enter") {
+      createTodoAtIndex(e, i);
+    }
+  }
+
+  function createTodoAtIndex(e, i) {
+    const newTodos = [...todos];
+    newTodos.splice(i + 1, 0, {
+      content: "",
+      isCompleted: false,
+    });
+    setTodos(newTodos);
+    setTimeout(() => {
+      document.forms[0].elements[i + 1].focus();
+    }, 0);
+  }
+
   return (
     <div className="app">
       <div className="header">
@@ -25,10 +43,16 @@ function App() {
       </div>
       <form className="todo-list">
         <ul>
-          <div className="todo">
-            <div className="checkbox" />
-            <input type="text" value="Todo one" />
-          </div>
+          {todos.map((todo, i) => (
+            <div className="todo">
+              <div className="checkbox" />
+              <input
+                type="text"
+                value={todo.content}
+                onKeyDown={(e) => handleKeyDown(e, i)}
+              />
+            </div>
+          ))}
         </ul>
       </form>
     </div>
